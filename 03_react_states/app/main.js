a46d106dafea64ebedbddfa7b28ce3c2d57f9d09
Andrii <andrii.karachevtsev@gmail.com>
2021-02-14 20:28:29 +0200

    states practise building simple Clock

---
+++ 03_react_states/app/main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Clock from './components/Clock';
+import './components/clock.css';
+
+
+ReactDOM.render(
+   <Clock></Clock>, 
+    document.getElementById('app')
+);
